94588ba83a0f739f50a1678c7dbddea81761bb00
Valentin Lab <valentin.lab@kalysto.org>
2013-04-14 19:06:35 +0200

    fix: pkg: some fixes to nasty bugs so that ``pip install colour`` actually works.

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ long_description = '\n\n'.join([open('README.rst').read(),
                                 open('CHANGELOG.rst').read(),
                                 open('TODO.rst').read()])
 
+## XXXvlab: Hacking distutils, not very elegant, but the only way I found
+## to get 'rgb.txt' to get copied next to the colour.py file...
+## Any suggestions are welcome.
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 setup(
     name='colour',
     version='%%version%%',
-    description='converts and manipulates various color representation (HSL, RVB, web, X11, ...)',
+    description='converts and manipulates various color representation '
+    '(HSL, RVB, web, X11, ...)',
     long_description=long_description,
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
         "Intended Audience :: Developers",
